f1638bfe4d889b9503c7bcc093e651eefa576ecd
Beata Tucholska <Beata.tucholska@rydoo.com>
2020-06-15 09:37:20 +0200

    Add project files.

---
+++ FunctionApp2/Function1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionApp2
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Lalala funkcja odpala.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var order = JsonConvert.DeserializeObject<Order>(requestBody);
+            
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Dzień dybry, {name}. This HTTP triggered function executed successfully.";
+
+            if (order != null)
+                responseMessage += $" Your order {order.OrderId} receved from{order.Email} for product {order.ProductId}";
+            else responseMessage += " Czego ode mnie chcesz?";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public string ProductId { get; set; }
+        public string Email { get; set; }
+    }
+}
